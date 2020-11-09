f0e147d807e489a837137bb4111da5f28344e012
Thibo Hoffman <47555318+ThiboHoffman@users.noreply.github.com>
2020-11-09 17:45:52 +0100

    TypeGameScherm
    - TypeGameScherm added

--- iosApps/iosApps/TypeGameController.swift
+++ iosApps/iosApps/TypeGameController.swift
@@ -9,10 +9,49 @@ import UIKit
 
 class TypeGameController: UIViewController {
 
+    
+    var stackView: UIStackView!
+    var casualBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpView()
+        setUpContraints()
+    }
+    
+    
+    func setUpView() {
+        
+        view.backgroundColor = .white
+        title = "Choose game type"
+        
+        casualBtn = UIButton()
+        casualBtn.setTitle("Casual", for: .normal)
+        casualBtn.setTitleColor(.blue, for: .normal)
+        casualBtn.layer.borderWidth = 1
+        casualBtn.layer.borderColor = UIColor.black.cgColor
+        casualBtn.layer.cornerRadius = 10
+        casualBtn.clipsToBounds = true
+        //casualBtn.addTarget(self, action: #selector(addPlayerTextField), for: .touchUpInside)
+        casualBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.spacing = 8.0
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.addArrangedSubview(casualBtn)
+        view.addSubview(stackView)
+    }
+    
+    func setUpContraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+        ])
+        
     }
     
 
